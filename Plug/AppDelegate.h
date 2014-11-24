4d698fef9cfae1fa9845b2ad85477eadaea3c467
Mubashhir <demo@Demos-MacBook-Pro.local>
2014-11-24 19:02:02 +0530

    Initial Commit

---
+++ Plug/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Plug
+//
+//  Created by Demo on 24/11/14.
+//  Copyright (c) 2014 theplugapp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Plug/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Plug
+//
+//  Created by Demo on 24/11/14.
+//  Copyright (c) 2014 theplugapp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
